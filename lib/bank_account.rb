e4527256ee06b05b385858b786efdd70c580d2fd
Christopher Spagnesi <christopherspagnesi@gmail.com>
2021-01-04 19:18:04 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,49 +1,32 @@
-describe "BankAccount" do 
+class BankAccount
+  # code here
+  attr_accessor :balance, :status
+  attr_reader :name
 
-  let(:avi) { BankAccount.new("Avi") }
-
-  it "can initialize a Bank Account" do
-    expect(avi).to be_a(BankAccount)
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
   end
 
-  it "initializes with a name" do
-    expect(avi.name).to eq("Avi")
+  def deposit(amount)
+    @balance += amount
   end
 
-  it "always initializes with balance of 1000" do
-    expect(avi.balance).to eq(1000)
+  def display_balance
+    "Your Balance is $#{@balance}."
   end
 
-  it "always initializes with a status of 'open'" do
-    expect(avi.status).to eq("open")
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
   end
 
-  it "can't change its name" do 
-    expect { avi.name = "Bob" }.to raise_error
+  def close_account
+    @status = "closed"
   end
 
-  it "can deposit money into its account" do 
-    expect(avi.balance).to eq(1000)
-    avi.deposit(1000)
-    expect(avi.balance).to eq(2000)
-  end
-
-  it "can display its balance" do 
-    expect(avi.display_balance).to eq("Your Balance is $#{avi.balance}.")
-  end
-
-  it "is valid with an open status and a balance greater than 0" do
-    @broke = BankAccount.new("Kat Dennings")
-    @broke.balance = 0
-    @closed = BankAccount.new("Beth Behrs")
-    @closed.status = "closed"
-    expect(avi.valid?).to eq(true)
-    expect(@broke.valid?).to eq(false)
-    expect(@closed.valid?).to eq(false)
-  end
-
-  it "can close its account" do 
-    avi.close_account
-    expect(avi.status).to eq("closed")
-  end
 end
